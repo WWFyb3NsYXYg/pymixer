176b70b4545ffd51aec07c758de8b24c9340ca61
yarestem <87089735+yarestem@users.noreply.github.com>
2022-04-12 17:26:41 +0300

    Обновлен интерфейс. Ускорен прием с Arduino

--- ArduinoCode/PyMixer/PyMixer.ino
+++ ArduinoCode/PyMixer/PyMixer.ino
@@ -1,112 +1,23 @@
-int reg = 0, flag = 0, ino = 0;
-#define BTN 5
-
-#define PIN_POT     A0
-void setup(){
-
+#define PIN_POT_1     A2
+#define PIN_POT_2     A1
+int val1, val2;
+void setup() {
   Serial.begin(115200);
-  pinMode(PIN_POT, INPUT);
-  pinMode(BTN,INPUT_PULLUP);
-  pinMode(13, OUTPUT);
-
-  pinMode(12, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(PIN_POT_1, INPUT);
+  pinMode(PIN_POT_2, INPUT);
 }
-
-
 void loop() {
-
-   if(digitalRead(BTN) == HIGH &&flag==0)//если кнопка нажата   
-     // и перемення flag равна 0 , то ... 
-     { 
-       reg++;   
-       flag=1; 
-         
-        //это нужно для того что бы с каждым нажатием кнопки 
-        //происходило только одно действие 
-        // плюс защита от "дребезга"  100% 
-          
-        if(reg>4)//ограничим количество режимов 
-        { 
-          reg=1;//так как мы используем только одну кнопку, 
-                    // то переключать режимы будем циклично 
-        } 
-       
-     } 
-       
-      if(digitalRead(BTN) == LOW &&flag==1)
-      //если кнопка НЕ нажата yyy
-     //и переменная flag равна - 1 ,то ... 
-     { 
-          
-        flag=0;//обнуляем переменную "knopka" 
-     } 
-      
-       
-       
-    if(reg==1)//первый режим 
-    { 
-      digitalWrite(13, LOW);
-digitalWrite(16, HIGH);
-digitalWrite(12, HIGH);
-digitalWrite(4, HIGH);
-fio();
-        
-        
-      //здесь может быть любое ваше действие 
-    } 
-    if(reg==2)//второй режим 
-    { 
-      digitalWrite(12, LOW);
-digitalWrite(13, HIGH);
-digitalWrite(4, HIGH);
-digitalWrite(16, HIGH);
-fio();
-        
-      //здесь может быть любое ваше действие 
-    } 
-      
-    if(reg==3)//третий режим 
-    { 
-      digitalWrite(4, LOW);
-digitalWrite(13, HIGH);
-digitalWrite(12, HIGH);
-digitalWrite(16, HIGH);
-fio();
-
-        
-      //здесь может быть любое ваше действие 
-    } 
-      
-    if(reg==4)//четвертый режим 
-    { 
-      digitalWrite(16, LOW);
-digitalWrite(12, HIGH);
-digitalWrite(13, HIGH);
-digitalWrite(4, HIGH);
-fio();
-
-      //здесь может быть любое ваше действие 
-    } 
-      
- }
-
-}
-
-void fio(){
-  if (analogRead(PIN_POT) != ino){
-  ino = analogRead(PIN_POT)/10.24;
-  Serial.print(reg);
- Serial.print(',');
-   if (ino == 1){
-    ino = 0;
-    Serial.print(ino);
+  if (val1 != float(map(analogRead(PIN_POT_1), 60, 850, 0, 100)) || val2 != float(map(analogRead(PIN_POT_2), 35, 1013, 0, 100))) {  
+    val1 = float(map(analogRead(PIN_POT_1), 60, 850, 0, 100));
+    val2 = float(map(analogRead(PIN_POT_2), 35, 1013, 0, 100));
+    val1 = float(constrain(val1, 0, 100));
+    val2 = float(constrain(val2, 0, 100));
+    Serial.print(val1);
+    Serial.print(',');
+    Serial.print(val2);
     Serial.print('\n');
-    delay(10);}
-    else {
-   Serial.print(ino);
-   Serial.print('\n');
-   delay(10);
-      }}
-  }
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(15);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(15);
+  }}
--- PythonScript/main.py
+++ PythonScript/main.py
@@ -1,50 +1,60 @@
 #pyinstaller --icon=app.ico --noconsole main.py
 from __future__ import print_function
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QComboBox
-from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5 import QtWidgets, uic #интерфейс
+from PyQt5.QtWidgets import QComboBox  #интерфейс
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo #serial port
 from PyQt5.QtCore import QIODevice
 from PyQt5.QtGui import QIcon #библиотека для иконки
-from Paradoxis import Sound #файл с проэктом
-from Paradoxis import Keyboard
+from Paradoxis import Sound #библиотека для звука
+from Paradoxis import Keyboard #библиотека для звука
 import subprocess
-
-valnm = 2
-
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("main.sk") # загружаем интерфейс
-ui.setWindowTitle("Beta Mixer") # устанавливам название файла
+ui.setWindowTitle("PyMixer") # устанавливам название файла
 ui.setWindowIcon(QIcon('icon.png')) # устанавливам иконку файла
 serial = QSerialPort()
-serial.setBaudRate(115200) # устанавливам BaudRate на 115200
+serial.setBaudRate(115200) # устанавливам BaudRate на 9600
 Sound.volume_set(0) # устанавливам звук на 0%
-ext_data = 0
+
+comb = QtWidgets.QComboBox()
+
+
+
+# действия при открытом порте
 def onRead():
     if not serial.canReadLine(): return     # выходим если нечего читать
     rx = serial.readLine()
     rxs = str(rx, 'utf-8').strip()
     data = rxs.split(',')
-    if data[0] == '1':
-        #print(data[1])
-        Sound.volume_set(int(data[1]))
-        ui.lcdM.display(int(data[1]))
-        
-    elif data[0] == '2':
-        #print(data[1])
+    #print(data)
+    Sound.volume_set(int(data[0]))
+    ui.lcdM.display(int(data[0]))
+
+    if ui.DialMode.currentText() == "Chrome":
         dial_chrome_set(int(data[1]))
         ui.lcdC.display(int(data[1]))
-    elif data[0] == '3':
-        #print(data[1])
+    elif ui.DialMode.currentText() == "AIMP":
         dial_aimp_set(int(data[1]))
         ui.lcdA.display(int(data[1]))
-    elif data[0] == '4':
-        #print(data[1])
+    elif ui.DialMode.currentText() == "TG / Viber":
+        dial_telegram_set(int(data[1]))
+        ui.lcdT.display(int(data[1]))
+    elif ui.DialMode.currentText() == "Another app":
         dial_app_set(int(data[1]))
         ui.lcdS.display(int(data[1]))
+
 # открыть порт
-comb = QtWidgets.QComboBox()
+def onOpen():
+    serial.setPortName(ui.comL.currentText())
+    serial.open(QIODevice.ReadWrite)
+
+# закрыть порт
+def onClose():
+    serial.close()
+
+#управление громкостью Chrome
 def dial_chrome_set(app_chrome):
     app_chrome = float(app_chrome) / 100
     sessions = AudioUtilities.GetAllSessions()
@@ -53,13 +63,18 @@ def dial_chrome_set(app_chrome):
         volume = session._ctl.QueryInterface(ISimpleAudioVolume)
         if session.Process and session.Process.name() == "chrome.exe":
           volume.SetMasterVolume(app_chrome, None)
-           
 
+#управление громкостью Aimp
+def dial_aimp_set(app_aimp):
+    app_aimp = float(app_aimp) / 100
+    sessions = AudioUtilities.GetAllSessions()
+    ui.lcdA.display(ui.dial_aimp.value())
+    for session in sessions:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        if session.Process and session.Process.name() == "AIMP.exe":
+          volume.SetMasterVolume(app_aimp, None)
 
-
-
-
-
+#управление громкостью Telegram
 def dial_telegram_set(app_telegram):
     prog = ui.clo.currentText() + ".exe"
     
@@ -70,7 +85,8 @@ def dial_telegram_set(app_telegram):
         volume = session._ctl.QueryInterface(ISimpleAudioVolume)
         if session.Process and session.Process.name() == prog:
           volume.SetMasterVolume(app_telegram, None)
-            
+
+#управление громкостью приложения на выбор
 def dial_app_set(app_app):
     prog = ui.sell.currentText()
     
@@ -82,21 +98,7 @@ def dial_app_set(app_app):
         if session.Process and session.Process.name() == prog:
           volume.SetMasterVolume(app_app, None)
 
-def dial_aimp_set(app_aimp):
-    app_aimp = float(app_aimp) / 100
-    sessions = AudioUtilities.GetAllSessions()
-    ui.lcdA.display(ui.dial_aimp.value())
-    for session in sessions:
-        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-        if session.Process and session.Process.name() == "AIMP.exe":
-          volume.SetMasterVolume(app_aimp, None)
-
-def onOpen():
-    serial.setPortName(ui.comL.currentText())
-    serial.open(QIODevice.ReadWrite)
-# закрыть порт
-def onClose():
-    serial.close()
+#заглушить Chrome
 def muteSoundC(val): 
     #print(ui.mute_chrome.checkState())
     if ui.mute_chrome.checkState() == 2:
@@ -108,6 +110,7 @@ def muteSoundC(val):
         dial_chrome_set(ui.dial_chrome.value())
         ui.lcdC.display(ui.dial_chrome.value())
 
+#заглушить Telegram
 def muteSoundT(val): 
     
     if ui.mute_tg.checkState() == 2:
@@ -119,6 +122,7 @@ def muteSoundT(val):
         dial_telegram_set(ui.dial_telegram.value())
         ui.lcdT.display(ui.dial_telegram.value())
 
+#заглушить выбраное приложение
 def muteSoundR(val): 
     
     if ui.mute_select.checkState() == 2:
@@ -130,6 +134,7 @@ def muteSoundR(val):
         dial_app_set(ui.dial_select.value())
         ui.lcdS.display(ui.dial_select.value())
 
+#заглушить AIMP
 def muteSoundA(val): 
     #print(ui.mute_chrome.checkState())
     if ui.mute_aimp.checkState() == 2:
@@ -141,14 +146,15 @@ def muteSoundA(val):
         dial_aimp_set(ui.dial_aimp.value())
         ui.lcdA.display(ui.dial_aimp.value())
 
+#заглушить выход
 def muteSound(val): 
-
     Sound.mute()
         #print(ui.mute_chrome.checkState())
     if ui.mute_main.checkState() == 2:
         ui.lcdM.display("--") 
     elif ui.mute_chrome.checkState() == 0:
         ui.lcdM.display(ui.dial_main.value())
+
 # обновить список портов
 def updateList():
     portList = []
@@ -157,6 +163,7 @@ def updateList():
     ui.comL.clear()
     ui.comL.addItems(portList)
 
+# обновить список приложений
 def updateListApp():
     ui.sell.clear()
     cmd = 'powershell "gps | where {$_.MainWindowTitle } | select Path'
@@ -175,40 +182,40 @@ def updateListApp():
             elif appList == "Viber.exe": i = 3
             elif appList == "ApplicationFrameHost.exe": i = 3
             else:ui.sell.addItem(appList)
-            
 
+#управление громкостью звука крутилка      
 def setSoundDial():
     #print(ui.dial_main.value()) # DeBug элемента Dial
     Sound.volume_set(ui.dial_main.value())
     ui.lcdM.display(ui.dial_main.value())
 
+#управление громкостью Chrome крутилка
 def dial_chrome_preset():
     dial_chrome_set(ui.dial_chrome.value())
 
+#управление громкостью Telegram крутилка
 def dial_telegram_preset():
     dial_telegram_set(ui.dial_telegram.value())
 
+#управление громкостью AIMP крутилка
 def dial_aimp_preset():
     dial_aimp_set(ui.dial_aimp.value())
 
+#управление громкостью выбраного приложения крутилка
 def dial_r_preset():
     dial_app_set(ui.dial_select.value())
     
+#кнопка reset 
 def res_funk():
     updateList()
     updateListApp()
 
-    
-def tv_val():
-    #print("SEC")
-    i = 8
 
-#sell
 
-updateListApp()
 # пошла программа
+updateListApp()
 ui.btn_r.clicked.connect(res_funk)
-ui.clo.currentIndexChanged.connect(tv_val)
+#ui.clo.currentIndexChanged.connect(funct.tv_val)
 serial.readyRead.connect(onRead)
 ui.mute_main.stateChanged.connect(muteSound)
 ui.mute_select.stateChanged.connect(muteSoundR)
@@ -222,7 +229,6 @@ ui.dial_aimp.valueChanged.connect(dial_aimp_preset)
 ui.dial_select.valueChanged.connect(dial_r_preset)
 ui.dial_telegram.valueChanged.connect(dial_telegram_preset)
 ui.dial_main.valueChanged.connect(setSoundDial)
-#comb.select_TV.connect(tv_val)
 updateList()
 
 ui.show()
